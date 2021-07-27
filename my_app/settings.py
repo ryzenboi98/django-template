f5e58de75d25c2abd4ee3db5aab14e269eac35ef
ryzenboi98 <joaopro23@gmail.com>
2021-07-27 21:11:34 +0100

    Removed debugging unnecessary information

--- my_app/settings.py
+++ my_app/settings.py
@@ -18,12 +18,11 @@ dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-print('GEUUGEUGUE' + str(os.getenv('secret_key')))
+
 SECRET_KEY = os.getenv('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
